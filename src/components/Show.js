1dcd8c3c22afcc502cfe022d2b07dd5437809994
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2021-07-12 21:50:01 -0400

    Update Show.js
    added update module link, and fixed id number

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import {Button} from 'react-bootstrap';
-import { Trash, Pencil } from 'react-bootstrap-icons';
+import { Trash } from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Update_Modal from './Update_Modal';
 
 const Show = () => {
     const url = 'https://60d4dcdcc6549200173a50fa.mockapi.io/Users'
-    const [users, setProduct] = useState(null)
+    const [users, setProduct] = useState(null);
+
 
     useEffect(() => {
         axios.get('/users')
         .then(response => {
             setProduct(response.data)
             console.log(response.data)
-         
         })
     }, [url])
         return (
@@ -21,15 +22,14 @@ const Show = () => {
                 {users && users.map((users, index) => {
                 return (
                 <tr className="book" key={index}>
-                    <td>{users.id}</td>
+                    <td>{index} </td>
                     <td>{users.name}</td>
                     <td>{users.email}</td>
                     <td>{users.gender}</td>
                     <td>{users.status}</td>
                     <td><img alt="avatar" src = {users.avatar}></img ></td>
                 <td>
-                <Button variant="info"><Pencil className="bi bi-trash" /></Button>
-               
+                <Update_Modal data = {users}/>
                 <Button variant="danger"><Trash className="bi bi-trash" /></Button>
                 </td>
               </tr>
